0e52cfadd3209f4afa00a9450bd773201bac2d09
GitHub Action <action@github.com>
2021-01-28 07:10:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/adebmbng/gorepogen/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/adebmbng/gorepogen/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.adebmbng.gorepogen.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adebmbng.gorepogen.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/adebmbng/gorepogen/services/MyProjectService.kt
+++ src/main/kotlin/com/github/adebmbng/gorepogen/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.adebmbng.gorepogen.services
 
+import com.github.adebmbng.gorepogen.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
